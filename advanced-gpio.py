42acc3bf9af639583728ffa387bbceaa4b7f0002
Sam Johnson-Lacoss <samjohnson-lacoss@Sams-MacBook-Pro.local>
2025-01-17 23:10:25 -0600

    tests

---
+++ advanced-gpio.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
+p.start(0)
+try:
+    while 1:
+        for dc in range(0, 101, 5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+        for dc in range(100, -1, -5):
+            p.ChangeDutyCycle(dc)
+            time.sleep(0.1)
+except KeyboardInterrupt:
+    pass
+p.stop()
+GPIO.cleanup()
