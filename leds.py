8e125aded19948bb11e432885e1ceaa3aee99fe8
Sam Johnson-Lacoss <samjohnson-lacoss@Sams-MacBook-Pro.local>
2025-01-17 22:34:41 -0600

    tests

--- leds.py
+++ leds.py
@@ -3,9 +3,9 @@ from gpiozero import LED
 from signal import pause
 import time
 
-RED = LED(17)
-GREEN = LED(22)
-BLUE = LED(24)
+RED = PWMLED(17)
+GREEN = PWMLED(22)
+BLUE = PWMLED(24)
 """
 We're using a 3-pin LED strip, so we can control each color individually.
 the pins are as follows:
@@ -18,13 +18,32 @@ GPIO Control:
 print([RED, GREEN, BLUE])
 # print([RED.pin, GREEN.pin, BLUE.pin])
 # print([RED.on(), GREEN.on(), BLUE.on()])
-RED.on()
-time.sleep(1)
-RED.off()
-GREEN.on()
-time.sleep(1)
-GREEN.off()
-BLUE.on()
-time.sleep(1)
-BLUE.off()
-pause()
+ 
+def color(r: float, g: float, b: float):
+    RED.value = r
+    GREEN.value = g
+    BLUE.value = b
+
+def on():
+    RED.on()
+    GREEN.on()
+    BLUE.on()
+
+def off():
+    RED.off()
+    GREEN.off()
+    BLUE.off()
+
+if __name__ == "__main__":
+    on()
+    time.sleep(3)
+    off()
+    time.sleep(3)
+    color(1, 0, 0)
+    time.sleep(3)
+    color(0, 1, 0)
+    time.sleep(3)
+    color(0, 0, 1)
+    time.sleep(3)
+    color(1, 1, 1)
+    time.sleep(3)
