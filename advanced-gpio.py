88e5f1b2c93123d81fd3d1f64a8117aed2ffb2f7
Sam Johnson-Lacoss <samjohnson-lacoss@Sams-MacBook-Pro.local>
2025-01-17 23:14:54 -0600

    tests

--- advanced-gpio.py
+++ advanced-gpio.py
@@ -6,11 +6,11 @@ led = PWMLED(17)
 max = 15
 try:
     while True:
-        for value in range(0, max, 5):
+        for value in range(0, max, 1):
             led.value = value / 100  # PWMLED value must be between 0 and 1
             print(value)
             sleep(0.1)
-        for value in range(max, -1, -5):
+        for value in range(max, -1, -1):
             print(value)
             led.value = value / 100
             sleep(0.1)
