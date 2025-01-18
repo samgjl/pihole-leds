ce1ddb752134b61eec5d52e9f25b3cf2c8f4af50
Sam Johnson-Lacoss <samjohnson-lacoss@Sams-MacBook-Pro.local>
2025-01-17 22:22:55 -0600

    init

---
+++ leds.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+from gpiozero import LED
+from signal import pause
+import time
+
+RED = LED(17)
+GREEN = LED(22)
+BLUE = LED(24)
+"""
+We're using a 3-pin LED strip, so we can control each color individually.
+the pins are as follows:
+GPIO Control:
+    Red     GPIO 17
+    Green   GPIO 22
+    Blue    GPIO 24
+"""
+
+print([RED, GREEN, BLUE])
+print([RED.pin, GREEN.pin, BLUE.pin])
+print([RED.on(), GREEN.on(), BLUE.on()])
+pause()
