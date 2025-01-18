285d95cb3faf7ae3221b86830a28a82f34618837
Sam Johnson-Lacoss <samjohnson-lacoss@Sams-MacBook-Pro.local>
2025-01-17 23:13:05 -0600

    tests

--- advanced-gpio.py
+++ advanced-gpio.py
@@ -7,8 +7,10 @@ try:
     while True:
         for value in range(0, 101, 5):
             led.value = value / 100  # PWMLED value must be between 0 and 1
+            print(value)
             sleep(0.1)
         for value in range(100, -1, -5):
+            print(value)
             led.value = value / 100
             sleep(0.1)
 except KeyboardInterrupt:
