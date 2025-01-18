493e6771e31f9bdd9e9a4ef4531ec6ffcd108ae4
Sam Johnson-Lacoss <samjohnson-lacoss@Sams-MacBook-Pro.local>
2025-01-17 22:54:25 -0600

    tests

--- ledstrip.py
+++ ledstrip.py
@@ -6,14 +6,5 @@ import time
 leds = LEDBoard(red=17, green=22, blue=24)
 print(leds)
 
-leds.on()
-time.sleep(1)
-leds.off()
-time.sleep(1)
-leds.red.on()
-time.sleep(1)
-leds.green.on()
-time.sleep(1)
-leds.blue.on()
-
-pause()
+while True:
+    leds.value = (1, 0, 0)
